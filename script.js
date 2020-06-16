af085d1171e2e651511344d9b5f74155d1354345
Andrey Baranov <andrey66r@yandex.ru>
2020-06-16 17:49:13 +0300

    Proetnaja 4 Ver. 2.0

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-btn');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
+let formElement = document.querySelector('.popup__form');
+const popup = document.querySelector('.popup');
+let userName = "ggg ";
+let userJob = " ";
 
-const togglePopup = function () {              // октрывает и закрывает окно popup
- const popup = document.querySelector('.popup');
+const togglePopup = function () { // октрывает попап, копирует данные из инпутов и закрывает попап
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
   popup.classList.toggle('popup_opened');
 }
 
-      // вставляем предварительные данные в окно popup
-  let profileName = document.querySelector('.profile__name');
-  let profileJob = document.querySelector('.profile__job');
-  let userName = profileName.textContent;
-  let userJob = profileJob.textContent;
-  let nameInput = document.querySelector('.popup__input_type_name');
-  let jobInput = document.querySelector('.popup__input_type_job');
-    nameInput.value = userName;
-    jobInput.value = userJob;
-
-      // вносит изменения данных в профиль пользователя
-let formElement = document.querySelector('.popup__form');
-function formSubmitHandler (evt) {
+function formSubmitHandler (evt) {  // вносит изменения данных в профиль пользователя
   evt.preventDefault();
     nameInput = document.querySelector('.popup__input_type_name');
     jobInput = document.querySelector('.popup__input_type_job');
@@ -30,8 +27,7 @@ function formSubmitHandler (evt) {
   togglePopup();
 }
 
-    // отвечают за действия пользователя
-editButton.addEventListener('click', togglePopup);
+editButton.addEventListener('click', togglePopup);   // обработчики событий
 closeButton.addEventListener('click', togglePopup);
 formElement.addEventListener('submit', formSubmitHandler);
 
